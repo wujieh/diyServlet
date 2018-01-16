fd2dd684521d6adb5020195de4d89f0c64808900
wujieh <wujieh@yonyou.com>
2018-01-16 20:47:06 +0800

    三目运算符替代if else

--- src/main/java/servlet/Service.java
+++ src/main/java/servlet/Service.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 
 public class Service extends HttpServlet {
     public void service(Request request, Response response){
-        String username = request.getParamter("user");
+        String username = (String)request.getParamter("user");
         response.htmlResponse("<html><head></head><body>This is my page<br><br>");
         response.htmlResponse("欢迎："+username+"  来到我的地盘</body></html>");
     }
